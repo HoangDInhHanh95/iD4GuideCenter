02a7e993532e9ce62fa8290ddf0c1668f6038dd1
HoangDInhHanh95 <kocamxuc114@gmail.com>
2025-11-21 11:55:33 +0700

    update đường link đúng

--- wp-content/themes/id4-guide-center/functions.php
+++ wp-content/themes/id4-guide-center/functions.php
@@ -1,44 +1,41 @@
 <?php
 
-wp_enqueue_style(
-    'id4-global-css',
-    get_stylesheet_directory_uri() . '/assets/css/global.css',
-    [],
-    filemtime(get_stylesheet_directory() . '/assets/css/global.css')
-);
+add_action('wp_enqueue_scripts', function () {
 
+    // Load GLOBAL CSS
+    $global = get_stylesheet_directory() . '/assets/css/global.css';
+    if (file_exists($global)) {
+        wp_enqueue_style(
+            'id4-global-css',
+            get_stylesheet_directory_uri() . '/assets/css/global.css',
+            array(),
+            filemtime($global)
+        );
+    }
 
-// code css cho trang chủ iD4GuideCenter
-$slug_trang_chu         = 'iD4GuideCenter'; // slug theo ảnh bạn gửi
-$rel_path_trang_chu     = '/assets/css/trangchu/trang-chu.css';
+    // Chỉ load TRANG CHỦ
+    if (is_front_page()) {
+        $home = get_stylesheet_directory() . '/assets/css/trangchu/trang-chu.css';
+        if (file_exists($home)) {
+            wp_enqueue_style(
+                'id4-home-css',
+                get_stylesheet_directory_uri() . '/assets/css/trangchu/trang-chu.css',
+                array('id4-global-css'), // ← global load trước
+                filemtime($home)
+            );
+        }
+    }
 
-$trang_chu_guide = get_stylesheet_directory() . $rel_path_trang_chu;
+    // JS GUIDE
+    $bs_id4_guide_js = get_stylesheet_directory() . '/assets/js/bs-id4-guide.js';
 
-// Không phụ thuộc domain; chỉ dựa vào slug/page hiện tại
-if (file_exists($trang_chu_guide) && is_page($slug_trang_chu)) {
-    // css trang chủ 
-    wp_enqueue_style(
-        'id4-trang-chu-iD4GuideCenter',
-        get_stylesheet_directory_uri() . $rel_path_trang_chu,
-        array('id4-globals'),                      // nếu CSS này cần biến trong tokens.css
-        filemtime($stackable_css_abs)             // cache-busting khi bạn sửa file
-    );
-}
-
-
-
-
-
-
-// fiel css
-// bs của id4 guide
-$bs_id4_guide_js = get_stylesheet_directory() . '/assets/js/bs-id4-guide.js';
-if (file_exists($bs_id4_guide_js)) {
-    wp_enqueue_script(
-        'id4-bs-guide-js',
-        get_stylesheet_directory_uri() . '/assets/js/bs-id4-guide.js',
-        ['jquery'], // có thể thêm ['jquery','gsap','swiper-cdn'] nếu cần
-        filemtime($bs_id4_guide_js),
-        true // load ở footer
-    );
-}
+    if (file_exists($bs_id4_guide_js)) {
+        wp_enqueue_script(
+            'id4-bs-guide-js',
+            get_stylesheet_directory_uri() . '/assets/js/bs-id4-guide.js',
+            ['jquery'],
+            filemtime($bs_id4_guide_js),
+            true
+        );
+    }
+}, 20); // <-- đóng hook
